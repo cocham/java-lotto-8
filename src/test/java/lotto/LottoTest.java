49356b4f1797d9a6515419a18f02c4015019cca3
cocham <mh020805@naver.com>
2025-10-29 16:44:05 +0900

    test: 구매 금액 예외 상황 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,10 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.Money;
+import lotto.exception.InvalidLottoMoneyException;
+import lotto.exception.InvalidNumberException;
+import lotto.exception.MinusException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,4 +27,26 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("구매 금액이 1000으로 안 나눠지면 예외가 발생한다.")
+    @Test
+    void 구매_금액이_1000으로_안_나눠지면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Money("300"))
+                .isInstanceOf(InvalidLottoMoneyException.class);
+    }
+
+    @DisplayName("구매 금액이 음수면 예외가 발생한다.")
+    @Test
+    void 구매_금액이_음수면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Money("-1000"))
+                .isInstanceOf(MinusException.class);
+    }
+
+    @DisplayName("구매 금액이 정수가 아니면 예외가 발생한다.")
+    @Test
+    void 구매_금액이_정수가_아니면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Money("1000.0"))
+                .isInstanceOf(InvalidNumberException.class);
+    }
+
+
 }
