10489cb5930e6ad20d9b013dd757c6cdca23f7ef
cocham <mh020805@naver.com>
2025-10-30 20:01:52 +0900

    test: InputParser 테스트

---
+++ src/test/java/lotto/InputParserTest.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import lotto.util.InputParser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputParserTest {
+    private final InputParser parser = new InputParser();
+
+    @DisplayName("쉼표를 기준으로 문자열을 분리하고 앞뒤 공백을 제거한다.")
+    @Test
+    void tokenizeNumbers() {
+        String input = " 1,2 , 3, 40 ,45 ";
+        List<String> result = parser.tokenizeNumbers(input);
+
+        assertThat(result).containsExactly("1", "2", "3", "40", "45");
+        assertThat(result).hasSize(5);
+    }
+}
