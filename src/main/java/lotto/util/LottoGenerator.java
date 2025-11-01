b7ed9b844ef88b712f517606d98b94a4defe0afa
cocham <mh020805@naver.com>
2025-11-01 20:50:18 +0900

    feat: 난수 생성에 전략 패턴(Strategy Pattern) 적용 및 테스트 용이성 확보

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -1,31 +1,29 @@
 package lotto.util;
-import camp.nextstep.edu.missionutils.Randoms;
+
 import lotto.domain.lotto.Lotto;
 import lotto.domain.Money;
+import lotto.domain.lotto.Lottos;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
-    public static final int MIN_LOTTO_NUMBER = 1;
-    public static final int MAX_LOTTO_NUMBER = 45;
-    public static final int PICK_COUNT = 6;
+    private final NumberGenerator numberGenerator;
 
-    //로또번호를 발행한다
-    public static List<Integer> issuanceLotto() {
-        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, PICK_COUNT);
+    public LottoGenerator(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
     }
 
-    //로또를 산다
-    public List<Lotto> buyLottos(Money money) {
+    public Lottos buyLottos(Money money) {
         int count = money.calculateLottoCount();
         List<Lotto> lottos = new ArrayList<>();
-        for (int c = 0; c < count; c++) {
-            List<Integer> lottoNumbers = issuanceLotto();
-            Lotto lotto = new Lotto(lottoNumbers);
-            lottos.add(lotto);
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> lottoNumbers = numberGenerator.generate();
+            lottos.add(new Lotto(lottoNumbers));
         }
-        return lottos;
+
+        return new Lottos(lottos);
     }
 
 }
---
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -0,0 +1,19 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomNumberGenerator implements NumberGenerator {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int PICK_COUNT = 6;
+
+    @Override
+    public List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(
+                MIN_LOTTO_NUMBER,
+                MAX_LOTTO_NUMBER,
+                PICK_COUNT
+        );
+    }
+}
