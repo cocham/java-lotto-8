5956a59a840dc16293fef34d05be1f6e97dd7171
cocham <mh020805@naver.com>
2025-11-03 18:07:47 +0900

    refactor: bonusNumber final 선언

--- src/main/java/lotto/domain/lotto/LottoBonusNumber.java
+++ src/main/java/lotto/domain/lotto/LottoBonusNumber.java
@@ -3,7 +3,7 @@ package lotto.domain.lotto;
 import static lotto.domain.lotto.LottoValidation.validateBonusNumber;
 
 public class LottoBonusNumber {
-    private int bonusNumber;
+    private final int bonusNumber;
 
     public LottoBonusNumber(int bonusNumber, WinningLotto winningLotto) {
         validateBonusNumber(bonusNumber, winningLotto);
